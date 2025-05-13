3b879eeb7ed63bbc87991fb760b51230b3ca6302
James Pearson <j.k.pearson87@gmail.com>
2025-05-13 17:37:04 +0100

    initial design patterns

---
+++ design_patterns/singleton/singletonBasic/Program.cs
@@ -0,0 +1,12 @@
+﻿Singleton singleton1 = Singleton.instance;
+Singleton singleton2 = Singleton.instance;
+
+
+sealed class Singleton
+{
+    public static Singleton instance {get;} = new();
+
+    private Singleton()
+    {
+    }
+}
---
+++ design_patterns/singleton/singletonLazyLoading/Program.cs
@@ -0,0 +1,11 @@
+﻿Singleton singleton1 = Singleton.instance;
+Singleton singleton2 = Singleton.instance;
+
+sealed class Singleton
+{
+    public static Singleton instance {get;} = new();
+
+    private Singleton()
+    {
+    }
+}
